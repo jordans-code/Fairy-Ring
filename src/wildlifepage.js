ddeae5252c3732d9469a5b7ae9d319a76e44ef53
The Danger <34897456+gigitygoog@users.noreply.github.com>
2019-06-27 16:00:01 +0300

    Now checks if garden was open on previous pages
    Revamped openall button, it will now check if the garden was already opened in this session

--- src/wildlifepage.js
+++ src/wildlifepage.js
@@ -39,15 +39,28 @@ function getlinks() { // returns array of links to gardens from wildlife page
 	var posts = $(".wall_postcontent");
 	var links = [];
 	for (i = 0; i < posts.length; i++) {
-		if (posts.eq(i).is(":visible")) {
 		var linkz = posts[i].getElementsByTagName("a")[0].toString();
-		links.push(linkz);
+		var linkid = linkz.split("=")[1];
+		var raw = window.sessionStorage.getItem("openedlinks");
+		var openedlinks = JSON.parse(raw);
+		if (posts.eq(i).is(":visible") && !(openedlinks.includes(linkid))) {
+			openedlinks.push(linkid);
+			console.log(openedlinks);
+			var stringify = JSON.stringify(openedlinks);
+			window.sessionStorage.setItem("openedlinks", stringify);
+			if (window.location.pathname == "/fbfairy/wildlifewall.php") {
+				if (window.sessionStorage.getItem("flid")) {
+					updatetotals(openedlinks.length - 1);
+				} else {
+					updatetotals(openedlinks.length);
+				}
+			}
+			links.push(linkz);
 		} else {
-			console.log(posts[i].getElementsByTagName("a")[0].toString() + " is blocking us, skipping.");
+			console.log(posts[i].getElementsByTagName("a")[0].toString() + " is blocking us or has already been opened, skipping.");
 		}
 	}
-	Final = [...new Set(links)]; // checks for duplicates
-	return Final;
+	return links;
 }
 
 
@@ -59,7 +72,20 @@ function next() { // next page button
 	}
 }
 
+function initialhtml() {
+	$('.wildlifetype').parent().eq(0).append("<div id='totals' style='width: 370px; background-color: #ddeedd; border: 1px solid #aabbaa; margin-left: 10px; margin-right: 10px;'><span id='totalsspan'>You have opened 0 gardens for this creature during this session.</span><br><br>Note: This only keeps track of gardens opened through the <b>open all</b> button, the button will also ignore gardens which have already been opened this session. To reset it, simply refresh this page.<br><br>If the open all button does not respond, it is because you have already opened all of the gardens on that page during this session.</div>");
+}
+
+function updatetotals(total) {
+	$('#totalsspan')[0].innerText = `You have opened ${total} gardens for this creature during this session.`;
+}
+
 function wallhandler(settings) { // handles wildlife page buttons
+		initialhtml();
+		if (settings.flid.length != 0) {
+			window.sessionStorage.setItem("flid", true);
+		}
+		window.sessionStorage.setItem("openedlinks", JSON.stringify([settings.flid]));
 		if ($(".mike_error").length == 0) { // If user is not on advert cooldown
 		$(".wall_form").children().eq(1).append('<input id="openall" class="inputsubmit" style="border:1px outset #007f00;background-color:#007f00;padding-top: 4px" type=button value="Open All Gardens"/>');
 		$(".wall_form").children().eq(1).append('<input id="next" class="inputsubmit" style="border:1px outset #007f00;background-color:#007f00;padding-top: 4px" type=button value="Next"/>');
