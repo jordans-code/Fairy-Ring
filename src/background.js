8a7894069c4117fcfbcf126e1b439958013565f7
thedanger1 <34897456+thedanger1@users.noreply.github.com>
2020-01-10 19:36:36 -0600

    Darkmode Listener/Injector
    CSS Injector addition

--- src/background.js
+++ src/background.js
@@ -1,5 +1,21 @@
+chrome.tabs.onUpdated.addListener(function(tabId, info, tab) {
+	do_something(tab);
+});
+
+function do_something(tab) {
+	var tabUrl = tab.url;
+	chrome.storage.sync.get(function(items) {
+		if (items.darkmode) {
+			chrome.tabs.insertCSS(tab.id, {
+				runAt: "document_start",
+				file: "css/dark-mode.css"
+			});
+		}
+	});	
+}
+
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    if (msg.action === "updateIcon") {
+	if (msg.action === "updateIcon") {
 		if (msg.value) {
 			chrome.browserAction.setIcon({
 				path: { "16": "icons/mushroom16notify.png"}
@@ -10,5 +26,6 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 			});
 			}
 	}
+	
 });
 
